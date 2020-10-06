55094907275bde94ccbb4f9dd963fb236d652edf
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-10-05 20:26:42 -0400

    functionality stopped working, so redid to make work again

--- A1/app/src/main/java/edu/neu/madcourse/numad20f_nicholasgalinski/ClickMessageFragment.java
+++ A1/app/src/main/java/edu/neu/madcourse/numad20f_nicholasgalinski/ClickMessageFragment.java
@@ -1,81 +1,24 @@
 package edu.neu.madcourse.numad20f_nicholasgalinski;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-import static edu.neu.madcourse.numad20f_nicholasgalinski.MainActivity.*;
-
-public class ClickMessageFragment extends Fragment {
-
-    private TextView pressedText;
+public class ClickMessageFragment extends AppCompatActivity {
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        pressedText = view.findViewById(R.id.textview_pressed);
-        pressedText.setText(getPressedText());
-
-        view.findViewById(R.id.button_a).setOnClickListener(messageOnClickListener);
-        view.findViewById(R.id.button_b).setOnClickListener(messageOnClickListener);
-        view.findViewById(R.id.button_c).setOnClickListener(messageOnClickListener);
-        view.findViewById(R.id.button_d).setOnClickListener(messageOnClickListener);
-        view.findViewById(R.id.button_e).setOnClickListener(messageOnClickListener);
-        view.findViewById(R.id.button_f).setOnClickListener(messageOnClickListener);
-
-        view.findViewById(R.id.click_previous).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(ClickMessageFragment.this)
-                        .navigate(R.id.action_MessageFragment_to_FirstFragment);
-            }
-        });
+    public void clickLetter(View v) {
+        String pressed = "Pressed: ";
+        Button btn = (Button) v;
+        TextView tv = (TextView) findViewById(R.id.textview_pressed);
+        tv.setText(pressed + btn.getText());
     }
-
-    private View.OnClickListener messageOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.button_a:
-                    setPressedText("Pressed: A");
-                    pressedText.setText(getPressedText());
-                    break;
-                case R.id.button_b:
-                    setPressedText("Pressed: B");
-                    pressedText.setText(getPressedText());
-                    break;
-                case R.id.button_c:
-                    setPressedText("Pressed: C");
-                    pressedText.setText(getPressedText());
-                    break;
-                case R.id.button_d:
-                    setPressedText("Pressed: D");
-                    pressedText.setText(getPressedText());
-                    break;
-                case R.id.button_e:
-                    setPressedText("Pressed: E");
-                    pressedText.setText(getPressedText());
-                    break;
-                case R.id.button_f:
-                    setPressedText("Pressed: F");
-                    pressedText.setText(getPressedText());
-                    break;
-            }
-        }
-    };
 }
