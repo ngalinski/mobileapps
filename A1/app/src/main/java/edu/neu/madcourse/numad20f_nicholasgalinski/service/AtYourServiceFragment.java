82eb5baf391492bddd64df4faefe6069931abbe9
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-10-25 21:17:33 -0700

    finalized service

--- A1/app/src/main/java/edu/neu/madcourse/numad20f_nicholasgalinski/service/AtYourServiceFragment.java
+++ A1/app/src/main/java/edu/neu/madcourse/numad20f_nicholasgalinski/service/AtYourServiceFragment.java
@@ -1,20 +1,16 @@
-package edu.neu.madcourse.numad20f_nicholasgalinski;
-
-import android.os.Bundle;
+package edu.neu.madcourse.numad20f_nicholasgalinski.service;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.LayoutInflater;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -23,18 +19,100 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Scanner;
 
+import edu.neu.madcourse.numad20f_nicholasgalinski.*;
+
+// started from WebServiceActivity start code
+// Async was deprecated - https://www.techyourchance.com/asynctask-deprecated/
+
 public class AtYourServiceFragment extends AppCompatActivity {
 
-    private TextView mTitleTextView;
-    private EditText mURLEditText;
+    private static final String TAG = "WebServiceActivity";
+
+    private String mURL;
+    private TextView quote_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_at_your_service);
 
-        mTitleTextView = (TextView)findViewById(R.id.result_textview);
-        mURLEditText = findViewById(R.id.url_edittext);
+        mURL = "https://zenquotes.io/api/random";
+        quote_result = (TextView)findViewById(R.id.result_quote);
     }
 
+    public void callWebserviceButtonHandler(View view){
+        PingWebServiceTask task = new PingWebServiceTask();
+        new Thread(task).start();
+
+        Toast.makeText(getApplicationContext(),"Quote obtained.", Toast.LENGTH_SHORT).show();
+    }
+
+    private class PingWebServiceTask implements Runnable{
+
+        @Override
+        public void run() {
+            String[] results = new String[2];
+            URL url = null;
+            try {
+                url = new URL(mURL);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                conn.connect();
+
+                InputStream inputStream = conn.getInputStream();
+                final String resp = convertStreamToString(inputStream);
+
+                JSONArray jsonarray = new JSONArray(resp);
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    JSONObject jsonobject = jsonarray.getJSONObject(i);
+                    String quote = jsonobject.getString("q");
+                    String author = jsonobject.getString("a");
+                    results[0] = quote;
+                    results[1] = author;
+                }
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG,"MalformedURLException");
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                Log.e(TAG,"ProtocolException");
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG,"IOException");
+                e.printStackTrace();
+            } catch (JSONException e) {
+                Log.e(TAG,"JSONException");
+                e.printStackTrace();
+            }
+
+            Button pingWeb = findViewById(R.id.visit_website);
+            pingWeb.setText("Website active. Click again to get a new quote.");
+
+            Button prompt = findViewById(R.id.get_quote);
+            prompt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TextView result_view = (TextView)findViewById(R.id.result_quote);
+                    result_view.setText(results[0]);
+                }
+            });
+
+            Button author = findViewById(R.id.get_author);
+            author.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TextView result_view = (TextView)findViewById(R.id.quote_author);
+                    result_view.setText(results[1]);
+                }
+            });
+        }
+    }
+
+    private String convertStreamToString(InputStream inputStream) {
+        Scanner s = new Scanner(inputStream).useDelimiter("\\A");
+        return s.hasNext() ? s.next().replace(",", ",\n") : "";
+    }
 }
